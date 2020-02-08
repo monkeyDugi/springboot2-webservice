0755a2ac6ea1e7f4b7e3f8b3c50a686d295d2295
monkeyDugi <lbd4946@gmail.com>
2020-02-09 00:09:50 +0900

    권한 수정

--- src/main/java/com/monkey/dugi/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/monkey/dugi/springboot/config/auth/dto/OAuthAttributes.java
@@ -67,7 +67,7 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.GUEST)
+                .role(Role.USER)
                 .build();
     }
 }
