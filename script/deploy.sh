9349fab9c7a4c267f5f8c58a113844a32a5a07fd
monkeyDugi <lbd4946@gmail.com>
2020-02-08 21:26:52 +0900

    주석 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -37,6 +37,6 @@ nohup java -jar \
         -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
         -Dspring.profiles.active=real \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 & # nohup 실행 시 codedeploy는 무한 대기하는 이슈 발생
-                                                 # 이 이유슬 해결하기 위해 nohup.out 파일을 표준 입출력용으로 별도로 사용
+                                                 # 이 이슈를 해결하기 위해 nohup.out 파일을 표준 입출력용으로 별도로 사용
                                                  # 이렇게 하지 않으면 nouhup.out 파일이 생기지 않고, codedeploy 로그에 표준 입출력이 출력 됨
                                                  # nohup이 끝나기 전까지 codedeploy도 끝나지 않으니 꼭 이렇게 해야만 함.
